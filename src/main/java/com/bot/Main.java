a419f2cd2af2b9bee9de2869f6464fa874a23353
ch941 <ch9412en@gmail.com>
2023-03-09 19:53:59 +0800

    Commit

--- src/main/java/com/bot/Main.java
+++ src/main/java/com/bot/Main.java
@@ -1,7 +1,12 @@
 package com.bot;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
     public static void main(String[] args){
+        String token = "OTk5OTcyMzE0ODI3NDcyOTU3.GAxm_N.SMKJZMVVo_9A5514KaFGgG4Srr4wsrcuFESD3Q";
+        JDA jda = JDABuilder.createDefault(token).build();
 
     }
 }
